7bbcf0bd1773abc896f1c40c3f371b44d7d408d7
Chris-D7 <constantincristian.draghici@ulbsibiu.ro>
2023-12-28 23:48:59 +0100

    Halfway through authentication(Just Login)

--- src/main/java/com/esp3/esp3/servlets/Users.java
+++ src/main/java/com/esp3/esp3/servlets/Users.java
@@ -3,6 +3,7 @@ package com.esp3.esp3.servlets;
 import com.esp3.esp3.common.CarDto;
 import com.esp3.esp3.common.UserDto;
 import com.esp3.esp3.ejb.UsersBean;
+import jakarta.annotation.security.DeclareRoles;
 import jakarta.inject.Inject;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -10,6 +11,11 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+
+@DeclareRoles({"READ_USERS", "WRITE_USERS"})
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = {"READ_USERS"}),
+        httpMethodConstraints = {@HttpMethodConstraint(value = "POST", rolesAllowed =
+                {"WRITE_USERS"})})
 
 @WebServlet(name = "Users", value = "/Users")
 public class Users extends HttpServlet {
